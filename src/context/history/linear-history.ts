fae151c42b09c18c8f70a1c9a7a77afe5bd73ddd
Jimin Park <pjm950@gmail.com>
2025-11-06 22:29:43 +0900

    Implemented LinearHistory.

--- src/context/history/linear-history.ts
+++ src/context/history/linear-history.ts
@@ -1,1 +1,51 @@
+import { MessageArray } from "llm-msg-io";
 import type { History } from "./type.js";
+import { ContextWindowConstraint } from "../type.js";
+
+export class LinearHistory implements History {
+    private history: MessageArray;
+    private committed_ind: number = 0;
+
+    constructor(history?: MessageArray) {
+        this.history = history ?? [];
+    }
+
+    async toMessageArray(constraint?: ContextWindowConstraint): Promise<MessageArray> {
+        if(!constraint) return this.history;
+
+        const { measure, max_tokens } = constraint;
+
+        let total_tokens = 0;
+        let end_index = this.history.length;
+
+        for(let i = this.history.length - 1; i >= 0; --i) {
+            const message = this.history[i];
+            const message_tokens = await measure.getNumTokens([message]);
+            total_tokens += message_tokens;
+
+            if(max_tokens < total_tokens) {
+                break;
+            }
+
+            end_index = i;
+        }
+
+        return this.history.slice(end_index);
+    }
+
+    async push(...messages: MessageArray): Promise<void> {
+        this.history.push(...messages);
+    }
+
+    async commit(): Promise<void> {
+        this.committed_ind = this.history.length;
+    }
+
+    async revert(): Promise<void> {
+        this.history.length = this.committed_ind;
+    }
+
+    async export(): Promise<MessageArray> {
+        return [...this.history];
+    }
+}
