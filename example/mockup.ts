b7a1840b2a4bca20b4c0119e6a9cbb8dd7643602
Jimin Park <pjm950@gmail.com>
2025-11-26 19:47:41 +0900

    Adding mockup and new APIs.

---
+++ example/mockup.ts
@@ -0,0 +1,8 @@
+const req = createRequest(...);
+// TODO: Applying history and tool calls to request.
+
+const llm = createLLMClient(...);
+const stream = await llm.chat(req, true);
+stream.on("content.delta", )
+
+const res = await stream.done();
