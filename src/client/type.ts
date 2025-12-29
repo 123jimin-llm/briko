a8748a9188c0a3a86fa10d8790fb7f55b687ecc4
Jimin Park <pjm950@gmail.com>
2025-12-29 18:14:06 +0900

    Made LLMEndpointParams a schema.

--- src/client/type.ts
+++ src/client/type.ts
@@ -1,14 +1,19 @@
+import { type } from "arktype";
 import type { StepResult, StepStream } from "llm-msg-io";
-import type { StepRequest } from "../request/index.js";
+
+import { exportType } from "../util/type.ts";
+import type { StepRequest } from "../request/index.ts";
 
 export interface LLMClient<ExtraStepParams extends object = object> {
     step<S extends boolean = false>(request: StepRequest<ExtraStepParams>, stream?: S): Promise<[S] extends [true] ? StepStream : StepResult>;
 }
 
-export type APIType = 'openai' | 'gemini';
+export const LLMAPIType = exportType(type("'openai'|'gemini'"));
+export type APIType = typeof LLMAPIType.infer;
 
-export interface LLMEndpointParams {
-    base_url: string;
-    api_key?: string;
-    api_type?: APIType;
-}
+export const LLMEndpointParams = exportType(type({
+    base_url: "string",
+    api_key: "string?",
+    api_type: LLMAPIType.optional(),
+}));
+export type LLMEndpointParams = typeof LLMEndpointParams.infer;
