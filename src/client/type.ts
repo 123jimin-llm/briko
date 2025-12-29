bad5e1d5eb3a35a5501997616f18178d7255decd
Jimin Park <pjm950@gmail.com>
2025-12-29 19:57:45 +0900

    Setting ArkType as peer dependency.

--- src/client/type.ts
+++ src/client/type.ts
@@ -1,19 +1,18 @@
 import { type } from "arktype";
 import type { StepResult, StepStream } from "llm-msg-io";
 
-import { exportType } from "../util/type.ts";
 import type { StepRequest } from "../request/index.ts";
 
 export interface LLMClient<ExtraStepParams extends object = object> {
     step<S extends boolean = false>(request: StepRequest<ExtraStepParams>, stream?: S): Promise<[S] extends [true] ? StepStream : StepResult>;
 }
 
-export const LLMAPIType = exportType(type("'openai'|'gemini'"));
+export const LLMAPIType = type("'openai'|'gemini'");
 export type APIType = typeof LLMAPIType.infer;
 
-export const LLMEndpointParams = exportType(type({
+export const LLMEndpointParams = type({
     base_url: "string",
     api_key: "string?",
     api_type: LLMAPIType.optional(),
-}));
+});
 export type LLMEndpointParams = typeof LLMEndpointParams.infer;
