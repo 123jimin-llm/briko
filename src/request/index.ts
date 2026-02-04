2920b783f5bb23d452aeeb7404d88a1e277692bc
Jimin Park <pjm950@gmail.com>
2026-02-04 23:30:24 +0900

    Let `createStepRequest` accept message array.

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,21 +1,48 @@
 export * from "./type.ts";
 
-import { asMessageArray, type MessageArrayLike } from "llm-msg-io";
-import type { StepRequest } from "./type.ts";
+import { asMessageArray, MessageArray, type MessageArrayLike } from "llm-msg-io";
+import type { StepRequest, StepRequestParams } from "./type.ts";
 import { applyTransforms } from "@jiminp/tooltool";
 
+/** Creates a `StepRequest` based on messages. */
 export function createStepRequest<ExtraParams extends object = object>(
-    base: Partial<StepRequest<ExtraParams>>,
+    messages: MessageArray,
+    extra_params?: ExtraParams,
+): StepRequest<ExtraParams>;
+
+/** Create a `StepRequest` based on a base request and messages. */
+export function createStepRequest<ExtraParams extends object = object>(
+    base: Partial<StepRequestParams<ExtraParams>>,
     messages: MessageArrayLike,
     extra_params?: ExtraParams,
+): StepRequest<ExtraParams>;
+
+export function createStepRequest<ExtraParams extends object = object>(
+    param0: Partial<StepRequestParams<ExtraParams>>|MessageArray,
+    param1: MessageArrayLike|ExtraParams|undefined,
+    param2?: ExtraParams,
 ): StepRequest<ExtraParams> {
-    const req: StepRequest<ExtraParams> = {
-        ...base,
-        messages: asMessageArray(messages),
-        use(...transforms) {
-            return applyTransforms(this, transforms);
-        },
-    };
+    let extra_params: ExtraParams|null = null;
+    let req: StepRequest<ExtraParams>;
+
+    if(Array.isArray(param0)) {
+        req = {
+            messages: param0,
+            use(...transforms) {
+                return applyTransforms(this, transforms);
+            }
+        };
+        extra_params = (param1 as ExtraParams|undefined) ?? null;
+    } else {
+        req = {
+            ...param0,
+            messages: asMessageArray(param1 as MessageArrayLike),
+            use(...transforms) {
+                return applyTransforms(this, transforms);
+            }
+        };
+        extra_params = param2 ?? null;
+    }
 
     if(extra_params != null) {
         req.extra_params = extra_params;
