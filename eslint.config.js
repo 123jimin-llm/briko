4e0623dffec311c333d8d85057a697551411abb0
Jimin Park <pjm950@gmail.com>
2026-02-11 23:17:15 +0900

    Added eslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,4 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-
-const config = tseslint.config([
-    {
-        ignores: ["dist/**"],
-    },
-    {
-        files: ["src/**/*.ts"],
-        extends: [
-            eslint.configs.recommended,
-            ...tseslint.configs.strict,
-        ],
-    },
-]);
-
-export default config;
+import config from "@jiminp/eslint-config";
+export default [...config];
